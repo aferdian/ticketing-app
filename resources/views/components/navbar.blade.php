07e5f4633900193cf943ce4aa216286a8a7f3ca2
Andri Ferdi <andri.ferdi15@gmail.com>
2025-10-22 13:56:00 +0700

    add all events & my events on navbar menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -101,7 +101,13 @@
                 });
             </script>
         @endif
-        @if (request()->routeIs(['home', 'events.show', 'checkout.form']))
+
+        @if (request()->routeIs(['home', 'admin.index', 'events.show', 'checkout.form']))
+            <a href="{{ route('home') }}"
+                class="hidden md:flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition">
+                <i class="ri-calendar-2-line"></i> 
+                <span class="text-sm font-medium text-gray-700 dark:text-gray-100">All Events</span>
+            </a>
             <a href="{{ route('orders.customers') }}"
                 class="hidden md:flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition">
                 <i class="ri-shopping-bag-line text-gray-600 dark:text-gray-100"></i>
@@ -151,11 +157,20 @@
                     @endif
                 </div>
 
-                @if (in_array(Auth::user()->role, ['customer', 'admin', 'superadmin']))
+                {{-- @if (in_array(Auth::user()->role, ['customer', 'admin', 'superadmin']))
                     <a href="{{ route('orders.customers') }}"
                         class="flex items-center px-4 py-3 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 md:hidden">
                         <i class="ri-shopping-bag-line mr-3 text-indigo-500"></i> My Orders
                     </a>
+                @endif --}}
+
+                @if (in_array(Auth::user()->role, ['admin']))
+                <div class="border-b border-gray-100 dark:border-gray-700">
+                    <a href="{{ route('admin.index') }}"
+                        class="flex items-center gap-2 px-4 py-2 text-gray-900 dark:text-gray-100 hover:bg-gray-600 dark:hover:bg-gray-500/30 transition">
+                        <i class="ri-calendar-event-line"></i> My Events
+                    </a>
+                </div>
                 @endif
 
                 <a href="{{ route('logout') }}"
