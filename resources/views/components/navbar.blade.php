f81523103dda086b932b58a2dacb6e0d631f7e80
Andri Ferdi <andri.ferdi15@gmail.com>
2025-12-01 14:14:21 +0700

    fix nav menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -102,17 +102,17 @@
             </script>
         @endif
 
-        @if (request()->routeIs(['home', 'admin.index', 'events.show', 'checkout.form']))
+        @if (request()->routeIs(['home', 'admin.index', 'events.show', 'checkout.form', 'orders.customers']))
             <a href="{{ route('home') }}"
                 class="hidden md:flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition">
                 <i class="ri-calendar-2-line"></i> 
                 <span class="text-sm font-medium text-gray-700 dark:text-gray-100">All Events</span>
             </a>
-            <a href="{{ route('orders.customers') }}"
+            {{-- <a href="{{ route('orders.customers') }}"
                 class="hidden md:flex items-center gap-2 px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition">
                 <i class="ri-shopping-bag-line text-gray-600 dark:text-gray-100"></i>
                 <span class="text-sm font-medium text-gray-700 dark:text-gray-100">My Orders</span>
-            </a>
+            </a> --}}
         @endif
 
         {{-- Kalau superadmin tampilkan notifikasi --}}
@@ -157,12 +157,12 @@
                     @endif
                 </div>
 
-                {{-- @if (in_array(Auth::user()->role, ['customer', 'admin', 'superadmin']))
+                @if (in_array(Auth::user()->role, ['customer', 'admin', 'superadmin']))
                     <a href="{{ route('orders.customers') }}"
-                        class="flex items-center px-4 py-3 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 md:hidden">
-                        <i class="ri-shopping-bag-line mr-3 text-indigo-500"></i> My Orders
+                        class="flex items-center gap-2 px-4 py-2 text-gray-900 dark:text-gray-100 hover:bg-gray-600 dark:hover:bg-gray-500/30 transition">
+                        <i class="ri-shopping-bag-line"></i> My Orders
                     </a>
-                @endif --}}
+                @endif
 
                 @if (in_array(Auth::user()->role, ['admin']))
                 <div class="border-b border-gray-100 dark:border-gray-700">
