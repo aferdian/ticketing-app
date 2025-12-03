096b6e4cbc3431633e2f40b77213ad5c6c0fd398
Andri Ferdi <andri.ferdi15@gmail.com>
2025-12-03 14:17:03 +0700

    hide create organization modal for superadmin

--- resources/views/modals/organizations/create.blade.php
+++ resources/views/modals/organizations/create.blade.php
@@ -1,4 +1,4 @@
-@if (count(auth()->user()->organizations) == 0)
+@if (auth()->user()->role != 'superadmin' && count(auth()->user()->organizations) == 0)
     <div id="organizationModal" class="fixed inset-0 z-50 flex items-center justify-center">
     @else
         <div id="organizationModal" class="fixed inset-0 z-50 hidden items-center justify-center">
